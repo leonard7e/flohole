7419354805d8b78564de7a616fe9b0ad9f70549c
Leonard Siebeneicher <leonard7e@online.de>
2025-04-09 08:43:04 +0200

    Now the user could tell the program how many half-notes he could place in any fraction of the flute. The fraction defaults to 1, and the position of the scale defaults to 9.0. For a musical scale, it does mean 9.0 halftones should fit into 0.5 of the length of the flute.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, parser::ValueSource};
+use clap::Parser;
 use std::collections::HashMap;
 
 #[derive(Parser, Debug)]
@@ -12,32 +12,39 @@ struct Args {
     /// Länge der Flöte
     length: f64,
 
-    /// Place midpoint of flute on this halftone
+    /// Die Position der Tonleitern in dem Segment (Fraction) der Flöte
     #[arg(short, long, default_value_t = 9.0)]
-    mid: f64,
+    position: f64,
 
-    /// Anzahl der Bohrlöcher
+    /// Anzahl der Bohrlöcher,
     #[arg(short, long, default_value_t = 6)]
     nholes: usize,
+
+    // Fraction bezeichnet ein Segment der Flöte.
+    #[arg(short, long, default_value_t = 0.5)]
+    fraction: f64,
 
     /// Layout der Bohrlöcher (dur6, moll6, dur7, moll7, dur8, moll8, chromatic, shakuhachi)
     #[arg(short, long, default_value = "major")]
     scale: String,
 
     /// Depth of the labium
-    #[arg(short, long, default_value_t = 8.0)]
+    #[arg(short, long, default_value_t = 6.0)]
     labium: f64,
 }
 
-fn intervall(mid: f64, notes: &[f64]) -> Vec<f64> {
-    notes.iter().map(|&note| 2.0f64.powf(-note / mid)).collect()
+fn fingerhole_locations(fraction: f64, pos: f64, notes: &[f64]) -> Vec<f64> {
+    notes
+        .iter()
+        .map(|&note| fraction.powf(note / pos))
+        .collect()
 }
 
-fn bohrlöcher(labium: f64, length: f64, mid: f64, skala: &[f64]) -> Vec<f64> {
-    let intervall_werte = intervall(mid, skala);
-    intervall_werte
+fn bohrlöcher(labium: f64, length: f64, fraction: f64, pos: f64, skala: &[f64]) -> Vec<f64> {
+    let fingerholes = fingerhole_locations(fraction, pos, skala);
+    fingerholes
         .iter()
-        .map(|&iv| labium + (length - labium) * iv)
+        .map(|&p| labium + (length - labium) * p)
         .collect()
 }
 
@@ -49,13 +56,15 @@ fn get_drill_layout() -> HashMap<&'static str, Vec<f64>> {
     drill_layout.insert("z-minor", vec![2.0, 3.0, 4.0, 7.0, 8.0, 11.0, 12.0, 14.0]);
     drill_layout.insert("minor-pentatonic", vec![3.0, 5.0, 7.0, 10.0, 12.0]);
     drill_layout.insert("major-pentatonic", vec![2.0, 4.0, 7.0, 9.0, 12.0]);
-    drill_layout.insert("uniform", vec![2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]);
+    drill_layout.insert("uniform", vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]);
     drill_layout
 }
 
+/*
 fn lerp(a1: f64, a2: f64, t: f64) -> f64 {
     (1.0 - t) * a1 + t * a2
 }
+*/
 
 fn main() {
     let args = Args::parse();
@@ -68,7 +77,13 @@ fn main() {
         .take(args.nholes)
         .cloned()
         .collect();
-    let loecher = bohrlöcher(args.labium, args.length, args.mid, &skala);
+    let loecher = bohrlöcher(
+        args.labium,
+        args.length,
+        args.fraction,
+        args.position,
+        &skala,
+    );
 
     println!("{:?}", loecher);
 }
